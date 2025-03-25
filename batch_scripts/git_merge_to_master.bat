fec41bb6789ac4b605a172e1610165f546fb7366
Mirko Lugano <mirko.lugano@outlook.it>
2025-03-25 12:31:02 +0100

    Changes from develop branch

--- batch_scripts/git_merge_to_master.bat
+++ batch_scripts/git_merge_to_master.bat
@@ -4,6 +4,6 @@ REM Run git commands
 cd ..
 cmd /c "git checkout master"
 cmd /c "git pull"
-cmd /c "git merge develop -m ""Merge develop branch to master"""
+cmd /c "git merge develop"
 cmd /c "git push"
 cmd /c "git checkout develop" 
