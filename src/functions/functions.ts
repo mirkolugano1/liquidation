bc4f3760c4318814e9001594b66a3475ce16a2b8
Mirko Lugano <mirko.lugano@outlook.it>
2025-03-31 17:17:16 +0200

    test change

--- src/functions/functions.ts
+++ src/functions/functions.ts
@@ -1,14 +1,9 @@
 import healthFactorCheckEngine from "../engines/healthFactorCheckEngine";
-import { app, Timer, InvocationContext } from "@azure/functions";
-
-async function testFunction(
-    timer: Timer,
-    context: InvocationContext
-): Promise<void> {
-    await healthFactorCheckEngine.testFunction(context);
-}
+import { app } from "@azure/functions";
 
 app.timer("testFunction", {
     schedule: "0 */5 * * * *", // Cron expression for every 5 minutes
-    handler: testFunction,
+    handler: async (myTimer, context) => {
+        await healthFactorCheckEngine.testFunction(context);
+    },
 });
